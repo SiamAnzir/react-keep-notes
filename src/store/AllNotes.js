09c91d239ab7c569d4ef932b085a69ba6224cd2c
SiamAnzir <siamanzir18@gmail.com>
2021-07-24 04:21:49 +0600

    [Added] editing field

--- src/store/AllNotes.js
+++ src/store/AllNotes.js
@@ -7,7 +7,8 @@ export const AllNotes = [
         description: "Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.",
         created_at: "Sat July 03 2021",
         favourite_note: false,
-        trash: false
+        trash: false,
+        editing:false
     },
     {
         id:uuidv4(),
@@ -16,6 +17,7 @@ export const AllNotes = [
         author:"Siam",
         created_at: "Sun May 02 2021",
         favourite_note: false,
-        trash:false
+        trash:false,
+        editing:false
     }
 ]
