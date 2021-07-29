95929da3cd0e9566a91942519177cdbdc5a34572
SiamAnzir <siamanzir18@gmail.com>
2021-07-29 23:32:01 +0600

    [Added] FontAwesome icon

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,17 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEdit, faPlusSquare, faStar, faTabletAlt, faTrash} from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
     return(
         <div className="sidebar">
-            <Link to="/addNote"> + Add Note</Link>
-            <Link to="/scratchPad">Scratchpad</Link>
-            <Link to="/">Notes</Link>
-            <Link to="/favourites">Favourites</Link>
-            <Link to="/trash">Trash</Link>
+            <br/>
+            <Link to="/addNote"> <FontAwesomeIcon icon={faPlusSquare} style={{ marginRight:".5rem"}}/> Add Note</Link>
+            <Link to="/scratchPad"><FontAwesomeIcon icon={faEdit} style={{ marginRight:".5rem"}}/> Scratchpad</Link>
+            <Link to="/"><FontAwesomeIcon icon={faTabletAlt} style={{ marginRight:".5rem"}}/> Notes</Link>
+            <Link to="/favourites"><FontAwesomeIcon icon={faStar} style={{ marginRight:".5rem"}}/> Favourites</Link>
+            <Link to="/trash"><FontAwesomeIcon icon={faTrash} style={{ marginRight:".5rem"}}/> Trash</Link>
         </div>
 
     )
