761aad2bac14f0ae81e5d914b67eba23bd3a9550
SiamAnzir <siamanzir18@gmail.com>
2021-07-30 02:59:41 +0600

    [Modified] notes value

--- src/store/AllNotes.js
+++ src/store/AllNotes.js
@@ -3,8 +3,8 @@ import { v4 as uuidv4 } from 'uuid';
 export const AllNotes = [
     {
         id:uuidv4(),
-        title:"Man must explore, and this is exploration at its greatest",
-        description: "Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.",
+        title:"Welcome To Keep Notes",
+        description: "keep Notes is a free, open-source notes app for the web. It is a demo project only, and does not integrate with any database or cloud. Your notes are saved in local storage and will not be permanently persisted, but are available for download. You can create your note and update it whenever it needs.",
         created_at: "Sat July 03 2021",
         favourite_note: false,
         trash: false,
@@ -12,9 +12,9 @@ export const AllNotes = [
     },
     {
         id:uuidv4(),
-        title:"Problems look mighty small",
-        description: "Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.",
-        created_at: "Sun May 02 2021",
+        title:"Features",
+        description: "Plain text notes - take notes in an IDE-like environment that makes no assumptions Linked notes - use uuid syntax to link to notes within other notes Theme - light and dark mode available Search notes - easily search all notes No database - notes are only stored in the browser's local storage and are available for download and export to you alone Copy Note - You can copy your note with a single button No tracking or analytics - 'nuff said GitHub integration - self-hosted option is available for auto-syncing to a GitHub repository",
+        created_at: "Sun July 27 2021",
         favourite_note: false,
         trash:false,
         editing:false
